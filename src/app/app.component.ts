61b592455e2edbe030520b4e3d6eff222f492aaf
juan esteban parra ducuara <juan.esteban.parra.ducuara@hotmail.com>
2021-04-03 13:00:05 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,6 +37,13 @@ export class AppComponent {
     this.zooService.llamadoHttp( tipo, sql ).subscribe(  //el subscribe hace que se espere respuesta
       (data: any) => {  //data es todo lo que devuelve como respuesta el llamado al webservice
         console.log(data); // se usan los datos ( en este caso se muestra la res. en la consola del navegador)
+        
+        if(data.success == true){ //valida si el webservide me responde de manera correcta
+          console.log(data.mensaje[0]); //si me repondio true en success, muestro en consola el primer dato
+        }
+        else{
+          console.log("hubo false en webservice");//si me resspondio fale hubo algo mal en el webservice
+        }
       },
       (error:any) => { //si hubo algun error en el llamado se guarda aqui en error
         console.log(error); //si hubo error se ejecuta este codigo y no el de arriba de data(en este caso se mostraria el error en consola)
