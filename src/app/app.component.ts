bc4d79488b65c89a78520c8b59e4899cddf18552
juan esteban parra ducuara <juan.esteban.parra.ducuara@hotmail.com>
2021-04-03 12:44:25 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ZooService } from 'src/app/zoo.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zooUnibague';
+
+  //se declara como atributo el servicio a usar para el llamado al webservice
+  zooService:ZooService;
+
+  respuesta:any;
+
+  constructor(http:HttpClient){
+    //se crea un nuevo objeto de la clase zooService que llama al webservice
+    this.zooService = new ZooService(http);
+
+    //se llama al metodo de ejemplo de uso del webservice
+    this.llamadoEjemplo();
+  }
+
+  //metodo de ejemplo para usar webservice
+  llamadoEjemplo(){
+    //se declara la variable tipo, que contiene el tipo de script que se le dara al webservice
+    var tipo = "select";
+
+    //se declara la variable el script, que contiene el tipo de script que 
+    //se le dara al webservice
+    var sql = "select * from tablaPrueba";
+
+    //se llama al metodo llamadoHttp del objeto zooService y se le entregam las 
+    //varibles tipo y sql
+    this.zooService.llamadoHttp( tipo, sql ).subscribe(  //el subscribe hace que se espere respuesta
+      (data: any) => {  //data es todo lo que devuelve como respuesta el llamado al webservice
+        console.log(data); // se usan los datos ( en este caso se muestra la res. en la consola del navegador)
+      },
+      (error:any) => { //si hubo algun error en el llamado se guarda aqui en error
+        console.log(error); //si hubo error se ejecuta este codigo y no el de arriba de data(en este caso se mostraria el error en consola)
+      }
+    );
+  }
+
 }
