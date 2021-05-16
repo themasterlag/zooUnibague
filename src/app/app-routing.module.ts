4eeebec83ed8aa88e761954e865f83df6b13bc6d
juan esteban parra ducuara <juan.esteban.parra.ducuara@hotmail.com>
2021-05-16 16:11:36 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VentaComponent } from "src/app/taquilla/venta/venta.component";
+import { DevolucionComponent } from "src/app/taquilla/devolucion/devolucion.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/venta', pathMatch: 'full' },
+  { path: 'venta', component: VentaComponent },
+  { path: 'devolucion', component: DevolucionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 import { HttpClientModule } from '@angular/common/http';
 import { HabitatsComponent } from './habitats/habitats.component';
 import { AnimalesComponent } from './animales/animales.component';
@@ -11,6 +13,9 @@ import { UsuariosComponent } from './usuarios/usuarios.component';
 import { TaquillaComponent } from './taquilla/taquilla.component';
 import { ReportesComponent } from './reportes/reportes.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { VentaComponent } from './taquilla/venta/venta.component';
+import { DevolucionComponent } from './taquilla/devolucion/devolucion.component';
 
 
 @NgModule({
@@ -21,12 +26,16 @@ import { LoginComponent } from './login/login.component';
     UsuariosComponent,
     TaquillaComponent,
     ReportesComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent,
+    VentaComponent,
+    DevolucionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NgbModule,
     HttpClientModule
   ],
   providers: [],
