04bde56036321e90a0562c43c5a0bedcdd20b840
mariajosevillalba <79494623+mariajosevillalba@users.noreply.github.com>
2021-05-20 17:51:27 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { VentaComponent } from "src/app/taquilla/venta/venta.component";
 import { DevolucionComponent } from "src/app/taquilla/devolucion/devolucion.component";
 import { LoginComponent } from './login/login.component';
-
+import {UsuariosComponent} from 'src/app/usuarios/usuarios.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/venta', pathMatch: 'full' },
   { path: 'venta', component: VentaComponent },
   { path: 'devolucion', component: DevolucionComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'usuarios', component: UsuariosComponent }
+
 ];
 
 @NgModule({
