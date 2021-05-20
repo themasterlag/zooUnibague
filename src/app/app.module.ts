2b387e280c41fba76c81ce9502c773c2f597420f
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2021-05-20 17:21:10 -0500

    Revert "."
    This reverts commit 0329daed2aea4496af23389af905530fceaee371.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { HttpClientModule } from '@angular/common/http';
 import { HabitatsComponent } from './habitats/habitats.component';
@@ -50,6 +51,7 @@ import { AsignarRolComponent } from './usuarios/asignar-rol/asignar-rol.componen
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    NgbModule,
     HttpClientModule
   ],
   providers: [],
