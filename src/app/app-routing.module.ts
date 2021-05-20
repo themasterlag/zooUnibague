af2f6a1b37ede2014285cb70bbca4b7e73d63c25
DuvanEspinosa <78386522+DuvanEspinosa@users.noreply.github.com>
2021-05-20 17:49:32 -0500

    image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { VentaComponent } from "src/app/taquilla/venta/venta.component";
 import { DevolucionComponent } from "src/app/taquilla/devolucion/devolucion.component";
 import { LoginComponent } from './login/login.component';
+import { AnimalesComponent} from './animales/animales.component'
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/venta', pathMatch: 'full' },
   { path: 'venta', component: VentaComponent },
   { path: 'devolucion', component: DevolucionComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'animales', component: AnimalesComponent }
 ];
 
 @NgModule({
