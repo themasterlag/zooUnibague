657fb75c8041089af00310fbbdd6cf4c408701a6
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2021-05-19 19:23:32 -0500

    login1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VentaComponent } from "src/app/taquilla/venta/venta.component";
 import { DevolucionComponent } from "src/app/taquilla/devolucion/devolucion.component";
+import { LoginComponent } from "src/app/login/login.component";
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/venta', pathMatch: 'full' },
+  { path: '', redirectTo: '/ventas', pathMatch: 'full' },
   { path: 'venta', component: VentaComponent },
-  { path: 'devolucion', component: DevolucionComponent }
+  { path: 'devolucion', component: DevolucionComponent },
+  { path : 'login', component: LoginComponent }
 ];
 
 @NgModule({
