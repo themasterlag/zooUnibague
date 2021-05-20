c183aef41003554344417b4b5791c10710cfb0d3
themasterlag <juan.esteban.parra.ducuara@hotmail.com>
2021-05-20 02:03:31 -0500

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VentaComponent } from "src/app/taquilla/venta/venta.component";
 import { DevolucionComponent } from "src/app/taquilla/devolucion/devolucion.component";
-import { LoginComponent } from "src/app/login/login.component";
+import { LoginComponent } from './login/login.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/ventas', pathMatch: 'full' },
+  { path: '', redirectTo: '/venta', pathMatch: 'full' },
   { path: 'venta', component: VentaComponent },
   { path: 'devolucion', component: DevolucionComponent },
-  { path : 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
