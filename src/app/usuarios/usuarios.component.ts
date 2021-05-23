cc0e2f1632c4024b56f520aea16efe43f2faa9bd
mariajosevillalba <79494623+mariajosevillalba@users.noreply.github.com>
2021-05-23 00:47:17 -0500

    .

--- src/app/usuarios/usuarios.component.ts
+++ src/app/usuarios/usuarios.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import {ServicioUsuariosService} from 'src/app/usuarios/servicio-usuarios.service';
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
   styleUrls: ['./usuarios.component.css']
 })
-export class UsuariosComponent implements OnInit {
+export class UsuariosComponent 
+{title = 'zooUnibague';
 
-  constructor() { }
+//se declara como atributo el servicio a usar para el llamado al webservice
+zooService:ServicioUsuariosService;
 
-  ngOnInit(): void {
-  }
+respuesta:any;
+
+constructor(http:HttpClient){
+  //se crea un nuevo objeto de la clase zooService que llama al webservice
+  this.zooService = new ServicioUsuariosService(http);
+
+  //se llama al metodo de ejemplo de uso del webservice
+  
+}
+
+//metodo de ejemplo para usar webservice
+llamadoRol(tipoUsuario:String, nombreUsuario:String, rol:String, personaExclusiva:boolean){
+  //se declara la variable tipo, que contiene el tipo de script que se le dara al webservice
+  var tipo = "insert";
+
+  //se declara la variable el script, que contiene el tipo de script que 
+  //se le dara al webservice
+  var sql = "insert into usuarios(tipoUsuario,nombreUsuario,rol,personaExclusiva) values ('" + tipoUsuario+ "','"+ nombreUsuario+ "','"+ rol + "','"+ personaExclusiva+"');";
+
+  //se llama al metodo llamadoHttp del objeto zooService y se le entregam las 
+  //varibles tipo y sql
+  this.zooService.llamadoHttp( tipo, sql ).subscribe(  //el subscribe hace que se espere respuesta
+    (data: any) => {  //data es todo lo que devuelve como respuesta el llamado al webservice
+      console.log(data); // se usan los datos ( en este caso se muestra la res. en la consola del navegador)
+      
+      if(data.success == true){ //valida si el webservide me responde de manera correcta
+        console.log(data.mensaje[0]); //si me repondio true en success, muestro en consola el primer dato
+      }
+      else{
+        console.log("hubo false en webservice");//si me resspondio fale hubo algo mal en el webservice
+      }
+    },
+    (error:any) => { //si hubo algun error en el llamado se guarda aqui en error
+      console.log(error); //si hubo error se ejecuta este codigo y no el de arriba de data(en este caso se mostraria el error en consola)
+    }
+  );
+}
+
 
 }
