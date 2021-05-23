cfd9316ada9630c911829d9440f2828c6f7afdb2
AlejaReyes0627 <aleja.reyes0627@hotmail.com>
2021-05-23 00:46:37 -0500

    login(sin funcionar creo)

---
+++ src/app/login/service/servicios.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ServiciosService 
+{
+  private url:string;
+
+  constructor(private http:HttpClient) 
+  {
+    this.url = "http://35.224.163.198:8081";
+  }
+  llamadoHttp( tipo:string, sql:string )
+  {
+    var body:FormData = new FormData();
+    body.set("tipo",tipo);
+    body.set("sql",sql);
+    var respuesta:any = this.http.post( this.url, body);
+    return respuesta;
+  }
+  
+}
