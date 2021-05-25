0534e9a3c5ae103fb0c350fabc1779dd4c8f173e
DuvanEspinosa <78386522+DuvanEspinosa@users.noreply.github.com>
2021-05-25 16:05:10 -0500

    Duvan

--- src/app/animales/registro-animales/registro-animales.component.ts
+++ src/app/animales/registro-animales/registro-animales.component.ts
@@ -1,15 +1,61 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { RegistroService } from 'src/app/usuarios/registrar-personas/registro.service';
 
 @Component({
   selector: 'app-registro-animales',
   templateUrl: './registro-animales.component.html',
   styleUrls: ['./registro-animales.component.css']
 })
-export class RegistroAnimalesComponent implements OnInit {
+export class RegistroAnimalesComponent
+{
 
-  constructor() { }
+  title = 'zooUnibague';
+  codigo: String;
+  nombre: String;
+  especie: String;
+  fechaNacimiento: String;
+  fechaIngreso: String;
+  descripcion: String;
+  zooService: RegistroService;
 
-  ngOnInit(): void {
+
+  constructor(http: HttpClient) 
+  {
+    this.codigo = "";
+    this.nombre = "";
+    this.especie = "";
+    this.fechaNacimiento = "";
+    this.fechaIngreso = "";
+    this.descripcion = "";
+    this.zooService = new RegistroService(http);
   }
+
+
+  insertarAnimales()
+  {
+    var tipo = "insert";
+    var sql = "INSERT INTO animales(codigo,nombre,especie,fechaNacimiento,fechaIngreso,descripcion) VALUES('" + this.codigo + "','" + this.nombre + "','" + this.especie + "','" + this.fechaNacimiento + "','" + this.fechaIngreso + "','" + this.descripcion  + "');";
+    this.zooService.llamadoHttp(tipo, sql).subscribe((data: any) =>
+    { 
+      console.log(data);
+
+      if (data.success == true) 
+      {
+        console.log(data.mensaje[0]);
+      }
+      else {
+        console.log("hubo false en webservice");
+      }
+
+    },
+      (error: any) => 
+      {
+        console.log(error);
+      }
+    );
+  }
+
+ 
 
 }
