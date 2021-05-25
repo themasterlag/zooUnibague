ebfe7dcec9ae50f129ff171e562480dc9199ed45
eflj2005 <eflj2005@gmail.com>
2021-05-24 23:37:31 -0500

    Confirmar

--- src/app/taquilla/venta/venta.component.ts
+++ src/app/taquilla/venta/venta.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { ZooService } from 'src/app/zoo.service';
 import { HttpClient } from '@angular/common/http';
+import { right } from '@popperjs/core';
 
 interface cards {
   id: number;
   titulo: String;
+  tipo:  String;
   precio: number;
   cantidad: number;
   estado: boolean;
@@ -39,21 +41,12 @@ export class VentaComponent implements OnInit {
     this.zooService = new ZooService(http);
 
     var sql = "select * from tickets";
-
-    this.zooService.llamadoHttp( "select", sql ).subscribe((data: any) => {
-     
+    this.zooService.llamadoHttp( "select", sql ).subscribe((data: any) => {    
       if(data.success == true){
-
-        
         for (let atributo in data.mensaje) {
           this.listaTickets.push(data.mensaje[atributo])
-        }
-
-        console.log(this.listaTickets,"Constructor");
-        
-        this.LLenarCardsDeTickets();
-
-        console.log(this.arreglo, "Organizado");
+        }            
+        this.lLenarCardsDeTickets();
       }
       else{
         console.log("hubo false en webservice");
@@ -67,37 +60,18 @@ export class VentaComponent implements OnInit {
   
 
   ngOnInit(): void {
-    var con = 0;        
-
-    // for (let j = 0; j < 3; j++) {
-    //   this.tickets = [];
-
-    //   for (let i = 0; i < 4; i++) {
-    //     con += 1;
-    //     this.tickets.push({
-    //       id:(con-1),
-    //       titulo:"Ticket " + (con),
-    //       precio: (con)*1000+100,          
-    //       cantidad:0,
-    //       estado:true
-    //     })
-    //   }
-    //   this.arreglo.push(this.tickets); 
-    // }
-
-
-    // console.log(this.arreglo, "Organizado");
 
   }
 
-  private LLenarCardsDeTickets(){
+
+  private lLenarCardsDeTickets(){
 
     var posicionEnBloque = 0;
     var bloqueTickets:cards[]=[];
 
     for (let ticket of this.listaTickets) {
      
-      var ticketCard:cards ={ id: ticket.id,  titulo: ticket.nombre,  precio: ticket.valor, cantidad: 0,  estado: true }
+      var ticketCard:cards ={ id: ticket.id,  titulo: ticket.nombre,  tipo: ticket.tipo, precio: ticket.valor, cantidad: 0,  estado: true }
 
       bloqueTickets.push(ticketCard);
       posicionEnBloque++;
@@ -140,9 +114,9 @@ export class VentaComponent implements OnInit {
       });
     }
     else if (caso ==1) {
-      this.listaSeleccionados.forEach(element => {
+      this.listaSeleccionados.forEach((element,index) => {
         if (element.id == id) {
-          respuesta = element
+          respuesta = { indice: index, ticket: element };
         }
       });
     }
@@ -151,8 +125,9 @@ export class VentaComponent implements OnInit {
   }
 
   public agregar(id:number){
-    var ticket = this.encontrar(id,0);
+    var ticket:any = this.encontrar(id,0);
     if (ticket != null) {
+      ticket.estado =false;
       this.listaSeleccionados.push(ticket);
     }
     else{
@@ -167,9 +142,12 @@ export class VentaComponent implements OnInit {
   }
 
   public eliminar(id:number){
-    var ticket = this.encontrar(id,1);
-    if (ticket != null) {
-      this.listaSeleccionados.splice(ticket,1);
+    var respuesta:any = this.encontrar(id,1);
+    if (respuesta != null) {
+      respuesta.ticket.estado =true;
+      respuesta.ticket.cantidad =0;
+      this.listaSeleccionados.splice(respuesta.indice,1);
+      this.calcularTotal();
     }
     else{
       Swal.fire({
@@ -187,6 +165,55 @@ export class VentaComponent implements OnInit {
     this.listaSeleccionados.forEach(element => {
       this.totalVenta += element.precio * element.cantidad;
     });
+  }
+
+
+  public verDetalleTicket(id:number){
+    var sql = "select * from habitats H inner join detallesTickets DT on H.id=DT.idHabitat where DT.idTicket = "+id;
+    this.zooService.llamadoHttp( "select", sql ).subscribe((data: any) => {    
+      if(data.success == true){
+
+        var ticket:any = this.encontrar(id,0);
+
+        var infoTickethtml:String=
+        "<div class='row'><div class='col-3 text-start'><strong>Nombre:</strong></div><div class='col-9 text-end'>" + ticket.titulo +"</div></div>"+
+        "<div class='row'><div class='col-3 text-start'><strong>Tipo:</strong></div><div class='col-9 text-end'>" + ticket.tipo +"</div></div>"+
+        "<div class='row'><div class='col-3 text-start'><strong>Valor:</strong></div><div class='col-9 text-end'>$ " + ticket.precio +"</div></div>"+
+        "<BR/>";
+
+        var listaHabitatsHtmml:string=
+        "<div class='row'>"+
+          "<div class='col-4'><strong>Nombre</strong></div>"+
+          "<div class='col-4'><strong>Tipo</strong></div>"+
+          "<div class='col-4'><strong>No. Animales</strong></div>"+
+        "</div>";
+        
+        for (let elemento in data.mensaje) {
+          listaHabitatsHtmml += 
+          "<div class='row'>"+
+            "<div class='col-4 text-start'>" + data.mensaje[elemento].nombre + "</div>"+
+            "<div class='col-4'>" + data.mensaje[elemento].tipo + "</div>"+
+            "<div class='col-4'>" + data.mensaje[elemento].cantidadMaxanimales + "</div>"+
+          "</div>";
+        }            
+        
+        Swal.fire({
+          title: '<strong><u>Información Del Ticket</u></strong>',
+          html:
+            infoTickethtml + listaHabitatsHtmml,
+          showCloseButton: true,
+          showConfirmButton: false
+        });
+
+      }
+      else{
+        console.log("hubo false en webservice");
+      }
+    },(error:any) => { console.log(error); });
+
+
+
+
   }
 
 }
